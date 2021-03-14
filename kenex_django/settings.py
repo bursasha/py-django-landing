25fd77a4998725e0200fd85ef27b01ee2a80f14d
bursasha <bursasha1201@gmail.com>
2021-03-14 13:14:04 +0100

    V1.0.2 Changing HOST to DB_HOST

--- kenex_django/settings.py
+++ kenex_django/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env()
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-HOST = os.getenv('HOST')
+DB_HOST = os.getenv('DB_HOST')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
